91d072d1d8980fbc1f1ed950e5336ca68eb40a38
Jamie Player <jamieplayer@gmail.com>
2017-12-12 15:05:11 +0000

    updated schema to match specification

--- server/models/question.js
+++ server/models/question.js
@@ -1,11 +1,35 @@
 const mongoose = require('mongoose');
 
 const questionSchema = mongoose.Schema({
+  _id: {
+    $oid: ObjectId,
+  },
+  submitted_at: Date.now(),
   title: String,
-  question: String,
-  user: { default: 'user', type: String },
-}, {
-  timestamps: true,
+  body: String,
+  votes: Number,
+  author: {
+    _id: {
+      $oid: ObjectId()
+    },
+    name: String,
+    avatar: String,
+  }, 
+  answers: [{
+    _id: {
+      $oid: String
+    },
+    submitted_at: Date.now(),
+    answer: String,
+    votes: Number,
+    author: {
+      _id: {
+        $oid: ObjectId()
+      },
+      name: String,
+      avatar: String,
+    }, 
+  }],
 });
 
 const questionModel = mongoose.model('Question', questionSchema);
