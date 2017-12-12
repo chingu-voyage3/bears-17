6efd46ee54a59aff7391bf19715a6c3d39831f12
Jamie Player <jamieplayer@gmail.com>
2017-12-12 21:37:28 +0000

    added timestamps back to question schema

--- server/models/question.js
+++ server/models/question.js
@@ -1,18 +1,23 @@
 const mongoose = require('mongoose');
 
-const questionSchema = mongoose.Schema({
-  submitted_at: Date.now(),
-  title: String,
-  body: String,
-  votes: Number,
-  author: {
-    _id: {
-      $oid: ObjectId(),
+const questionSchema = mongoose.Schema(
+  {
+    submitted_at: Date.now(),
+    title: String,
+    body: String,
+    votes: Number,
+    author: {
+      _id: {
+        $oid: ObjectId(),
+      },
+      name: String,
+      avatar: String,
     },
-    name: String,
-    avatar: String,
   },
-});
+  {
+    timestamps: { createdAt: 'submitted_at', updatedAt: 'updated_at' },
+  },
+);
 
 const questionModel = mongoose.model('Question', questionSchema);
 
