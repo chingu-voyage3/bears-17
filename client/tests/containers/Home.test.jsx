59fd7d60929b23ca04171abf0b80191d9cb2b38b
Zsolt Meszaros <zsolt@sowad.com>
2017-12-14 23:20:46 +0000

    Add home container

---
+++ client/tests/containers/Home.test.jsx
@@ -0,0 +1,10 @@
+import Home from 'Containers/Home';
+
+describe('Home', () => {
+  it('should exist', () => {
+    expect(Home).toBeDefined();
+  });
+
+  it('should render the home page');
+  it('should load the questions from the API after page load');
+});
