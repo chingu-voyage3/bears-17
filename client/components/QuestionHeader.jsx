63c57247832467da9f5736ccf98aee1e63bff4ef
Zsolt Meszaros <zsolt@sowad.com>
2017-12-15 00:14:38 +0000

    Add QuestionHeader component

---
+++ client/components/QuestionHeader.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const QuestionHeader = props => (
+  <div className="page__header">
+    <h2 className="page__title">{props.title}</h2>
+  </div>
+);
+
+QuestionHeader.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+export default QuestionHeader;
