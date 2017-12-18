91179ad2e6e22c4aafe4fb2c8ec3d5e486e05a92
Jamie Player <jamieplayer@gmail.com>
2017-12-18 11:44:29 +0000

    add false return if question is not found

--- server/controller/answers.js
+++ server/controller/answers.js
@@ -3,9 +3,14 @@ const Answer = require('../models/answer.js');
 
 exports.addAnswer = async (ctx) => {
   await Question.findById(ctx.request.body._id)
-    .then(() => {
-      const answer = new Answer(ctx.request.body);
-      return answer.save();
+    .then((res) => {
+      if (res) {
+        const answer = new Answer(ctx.request.body);
+        return answer.save();
+      }
+
+      ctx.body = false;
+      return ctx.body;
     });
 
   await Answer.find({ question_id: ctx.request.body._id })
