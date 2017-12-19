1dc0961857a75f44e52399a115863424e6116a40
Jamie Player <jamieplayer@gmail.com>
2017-12-19 22:32:32 +0000

    added form validation middleware for answer post route

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const Router = require('koa-router');
 const QuestionController = require('./controller/index.js');
 const AnswerController = require('./controller/answers.js')
 
+
+
 const app = new Koa();
 const router = new Router();
 
@@ -13,7 +15,7 @@ const port = process.env.API_PORT || 3000;
 // Promise Library for mongoose
 mongoose.Promise = require('bluebird');
 
-mongoose.connect(process.env.DB_URL, { useMongoClient: true })
+mongoose.connect(process.env.TEST_DB, { useMongoClient: true })
   .then((res) => {
     console.log('Mongoose connected');
   })
@@ -31,7 +33,7 @@ router
   .get('/api/questions', QuestionController.getQuestions)
   .get('/api/questions/random/:limit?', QuestionController.getRandomQuestions)
   .get('/api/question/:id', QuestionController.getId)
-  .post('/api/answer', AnswerController.addAnswer);
+  .post('/api/answer', AnswerController.validateAnswer, AnswerController.addAnswer);
 
 app
   .use(router.routes())
