5dfe84cb806f49dd9b6d0fca83f02393af2cbb00
Jamie Player <jamieplayer@gmail.com>
2017-12-19 22:33:24 +0000

    form validation middleware with trim and joi

--- server/controller/answers.js
+++ server/controller/answers.js
@@ -1,22 +1,62 @@
+const mongoose = require('mongoose');
 const Question = require('../models/question.js');
 const Answer = require('../models/answer.js');
+const trim = require('deep-trim-node');
+const Joi = require('joi');
+const ObjectId = require('mongoose').Types.ObjectId;
+Joi.objectId = require('eko-joi-objectid')(Joi, ObjectId);
 
 exports.addAnswer = async (ctx) => {
-  await Question.findById(ctx.request.body.question_id)
+  const { question_id: questionId } = ctx.answer;
+
+  await Question.findById(questionId)
     .then((res) => {
-      if (res) {
-        const answer = new Answer(ctx.request.body);
-        return answer.save();
+      if (!res) {
+        ctx.body = {
+          err: 'QuestionID does not exist on the DB',
+        };
+        return ctx.body;
       }
-
-      ctx.body = false;
+      const answer = new Answer(ctx.request.body);
+      ctx.body = answer.save();
+      return ctx.body;
+    })
+    .catch((err) => {
+      ctx.body = {
+        err,
+        message: 'Internal DB error, please try again',
+      };
       return ctx.body;
     });
+};
 
-  await Answer.find({ question_id: ctx.request.body._id })
-    .then((res) => {
-      const result = [res];
-      ctx.body = result;
-      return ctx.body;
+exports.validateAnswer = async (ctx, next) => {
+  const answer = trim(ctx.request.body);
+
+  const schema = Joi.object()
+    .keys({
+      question_id: Joi.objectId()
+        .required(),
+      body: Joi.string(),
+      author: Joi.object({
+        _id: Joi.objectId(),
+        name: Joi.string(),
+        avatar: Joi.string(),
+      }),
     });
+
+  const result = await Joi.validate(answer, schema)
+    .then(res => res)
+    .catch((err) => {
+      console.error(err);
+      return { err, message: 'error validating your answer, please try again.' };
+    });
+
+  if (result.err) {
+    ctx.body = result;
+    return ctx.body;
+  }
+
+  ctx.answer = result;
+  return next();
 };
