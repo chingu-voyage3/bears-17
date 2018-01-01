8ef428af187f0913559cf7b63e0025e20ae0b53f
Jamie Player <jamieplayer@gmail.com>
2018-01-01 16:50:43 +0000

    updated userSchema to incldue password and automatic hashing on save

--- server/models/user.js
+++ server/models/user.js
@@ -1,13 +1,47 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const SALT_WORK_FACTOR = 10;
 
 const userSchema = mongoose.Schema({
   name: { type: String, required: true },
+  password: { type: String, require: true },
   email: String,
   avatar: String,
   country: String,
   member_since: { type: Date, default: Date.now },
   introduction: String,
 });
+
+userSchema.pre(save, function saltPass (next) {
+  const user = this;
+
+  if (!user.isModified('password')) return next();
+
+  // generate a salt
+
+  bcrypt.getSalt(SALT_WORK_FACTOR, (err, salt) => {
+    if (err) return next(err);
+
+    // has the password using our new salt
+
+    bcrypt.hash(user.password, salt, (hashErr, hash) => {
+      if (hashErr) {
+        return next(err);
+      }
+
+      // override he cleartext password with the hashed one
+      user.password = hash;
+      return next();
+    });
+  });
+});
+
+userSchema.methods.verifyPassword = function verifyPassword(password, cb) {
+  return bcrypt.compare(password, this.password, (err, isMatch) => {
+    if (err) return cb(err);
+    return cb(null, isMatch);
+  });
+};
 
 const userModel = mongoose.model('User', userSchema);
 
