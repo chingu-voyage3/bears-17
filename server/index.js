c3ccca1b438ff175d6f96cc4679ec94fbbe39b37
OStefani <ostefanishina@gmail.com>
2018-01-04 18:02:54 +0300

    mark-question-as-spam

--- server/index.js
+++ server/index.js
@@ -38,6 +38,7 @@ router
   .get('/api/answers/:id', AnswerController.findAnswersById)
   .post('/api/answer', AnswerController.validateAnswer, AnswerController.addAnswer)
   .post('/api/answer/:id/flag', AnswerController.flag);
+  .post('/api/questions/:id/spam', QuestionController.markSpam);
 
 app
   .use(router.routes())
