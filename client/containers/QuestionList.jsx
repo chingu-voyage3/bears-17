7d943d872a1f68fde696302ba2020831095729b4
Jamie Player <jamieplayer@gmail.com>
2018-01-19 15:11:03 +0000

    created new container questionList

---
+++ client/containers/QuestionList.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class QuestionList extends {
+
+  render () {
+    return (
+        <div>
+          Hello World
+        </div>
+    );
+  };
+}
+
+export default QuestionList;
