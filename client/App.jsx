5589ad11b6204cb1435ea3a0a57ddcabc6f14939
Jamie Player <jamieplayer@gmail.com>
2018-01-19 09:29:37 +0000

    Added routes for authentication + creating questions

--- client/App.jsx
+++ client/App.jsx
@@ -6,13 +6,21 @@ import Footer from 'Components/Footer';
 
 import Home from 'Containers/Home';
 import Question from 'Containers/Question';
+import Profile from 'Containers/Profile';
+import Login from 'Containers/Login';
+import Register from 'Containers/Register';
+import CreateQuestion from 'Containers/CreateQuestion';
 
 const routes = () => (
   <Router>
     <div>
       <Header />
       <Switch>
+        <Route path="/profile" component={Profile} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
         <Route path="/question/:id" component={Question} />
+        <Route path="/create" component={CreateQuestion} />
         <Route path="/" component={Home} />
       </Switch>
       <Footer />
