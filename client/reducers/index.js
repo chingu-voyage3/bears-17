a41bf6f0e67c26a428b8d9d923cca8d9c3817a3b
Jamie Player <jamieplayer@gmail.com>
2018-02-07 14:14:44 +0000

    imported and combined userReducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
 
 import questionReducer from './questionReducer.js';
+import userReducer from './userReducer.js';
 
 export default combineReducers({
-  questionReducer,
+  questionReducer, userReducer,
 });
