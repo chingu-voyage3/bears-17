e35bc7bd7bfe9f3d9fc8e9d8501d48498fb3350b
Jamie Player <jamieplayer@gmail.com>
2018-02-07 14:12:09 +0000

    replaced anchor tags with react-router-dom Link components. Was previously rerendering app with hrefs

--- client/components/Navigation.jsx
+++ client/components/Navigation.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 const LINKS = [
   { label: 'Home', href: '/' },
@@ -6,18 +8,36 @@ const LINKS = [
   { label: 'Login', href: '/login' },
   { label: 'Register', href: '/register' },
   { label: 'Profile', href: '/profile' },
+  { label: 'Logout', href: '/logout' },
 ];
 
-const Navigation = () => (
-  <nav aria-label="Site links">
-    <ul className="main-nav">
-      {LINKS.map(link => (
-        <li key={link.label} className="main-nav__item">
-          <a className="main-nav__link" href={link.href}>{link.label}</a>
-        </li>
-      ))}
-    </ul>
-  </nav>
+const loggedInNavs = ['Profile', 'Logout'];
+const loggedOutNavs = ['Login', 'Register'];
+
+const linkFilter = (array1, array2) => (
+  array1.filter(e => array2.indexOf(e.label) === -1)
 );
 
+const Navigation = (props) => {
+  const NavLinks = props.loggedIn
+    ? linkFilter(LINKS, loggedOutNavs)
+    : linkFilter(LINKS, loggedInNavs);
+
+  return (
+    <nav aria-label="Site links">
+      <ul className="main-nav">
+        {NavLinks.map(link => (
+          <li key={link.label} className="main-nav__item">
+            <Link to={link.href} className="main-nav__link">{link.label}</Link>
+          </li>
+      ))}
+      </ul>
+    </nav>
+  );
+};
+
 export default Navigation;
+
+Navigation.propTypes = {
+  loggedIn: PropTypes.bool.isRequired,
+};
